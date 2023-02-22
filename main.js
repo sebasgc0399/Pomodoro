6724b0962cfbf77bd28986642c22dacd1b25d9b8
Sebastian <sebasgc0399@gmail.com>
2023-02-22 15:41:43 -0500

    main.js

---
+++ main.js
@@ -0,0 +1,88 @@
+
+const tasks = [];
+let time = 0;
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const bAdd = document.querySelector('#bAdd');
+const itTask = document.querySelector('#itTask');
+const form = document.querySelector('#form');
+const taskName = document.querySelector('#time #taskName')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    if(itTask.value != ''){
+        createTask(itTask.value);
+        itTask.value = '';
+        renderTask();
+    }
+});
+
+const createTask = (value) => {
+
+    const newTask = {
+        id: (Math.random() * 100).toString(36).slice(3),
+        title: value,
+        completed: false
+    }
+
+    tasks.unshift(newTask);
+}
+
+const renderTask = () => {
+    const html = tasks.map(task => {
+        return `
+            <div class="task">
+                <div class="completed">${task.completed ? `<span class="done">Done</span>` : `<button class="start-button" data-id="${task.id}">Start</button>`}</div>
+                <div class="title">${task.title}</div>
+            </div>
+        `;
+    });
+
+    const tasksContainer = document.querySelector('#tasks')
+    tasksContainer.innerHTML = html.join('');
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+
+    startButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if(!timer){
+                const id = button.getAttribute('data-id');
+                startButtonsHandler(id);
+                button.textContent = "In progress ...";
+            }
+        });
+    });
+}
+
+const startButtonsHandler = (id) => {
+    time = 25 * 60;
+    current = id;
+    const taskIndex = tasks.findIndex(task => task.id == id);   
+    taskName.textContent = tasks[taskIndex].title;
+
+    timer = setInterval(() => {
+        timeHandler(id);
+    }, 1000);
+} 
+
+const timeHandler = (id) => {
+    time--;
+    renderTime();
+
+    if(time == 0){
+        clearInterval(timer);
+        current = null;
+        taskName.textContent = "";
+        renderTime();
+    }
+}
+
+const renderTime = () => {
+    const timeDiv = document.querySelector('#time #value');
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+    timeDiv.textContent = `${minutes < 10 ? '0': ''}${minutes}:${seconds < 10 ? '0': ''}${seconds}`
+}
